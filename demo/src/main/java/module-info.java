4217b677aa1428b30d4e82e544c5b74d47991ee0
Ylljete Kicaj <118022552+ylljetakicaj@users.noreply.github.com>
2023-05-28 16:22:34 +0300

    Add files via upload

---
+++ demo/src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.example.demo {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.demo to javafx.fxml;
+    exports com.example.demo;
+}
